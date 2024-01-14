5e0233487ea44e9983f79eb4f6272bdefaa68485
Филатов А. В. <v1rus1885@mail.ru>
2024-01-14 17:10:51 +0300

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Parameter: $param"
+>>>>>>> a7452bb (git-rebase 1)
     count=$(( $count + 1 ))
 done
 
